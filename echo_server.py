e186efb15ecf0c417e545a3bfe70d8362b0495af
Gustav Svensk <grulfen3@gmail.com>
2013-09-24 22:26:33 -0300

    Made the echo server threaded

--- echo_server.py
+++ echo_server.py
@@ -1,8 +1,30 @@
 """ echo server program for elo322 - Redes de Computadores """
 import socket
+import threading
 import logging
 
 logging.basicConfig(filename="server.log", level=logging.INFO)
+
+
+class client_thread(threading.Thread):
+    """ Echos the message from a client connection """
+
+    def __init__(self, client_socket):
+        super().__init__()
+        self.socket = client_socket
+        self.addr = self.socket.getpeername()
+        logging.info("Connection from {0}".format(self.addr))
+
+    def run(self):
+        message_part = self.socket.recv(1024)
+        message = b""
+        while message_part:
+            message += message_part
+            message_part = self.socket.recv(1024)
+        logging.info("Got '{0}' from {1}".format(message.decode("utf-8"),
+                                                 self.addr))
+        self.socket.send(message)
+        self.socket.close()
 
 
 def main():
@@ -19,19 +41,13 @@ def main():
     listen_socket.listen(5)
     while True:
         try:
-            client_socket, addr = listen_socket.accept()
+            client_socket, _ = listen_socket.accept()
         except KeyboardInterrupt:
             return
-        logging.info("client_socketection from {0}".format(addr))
-        message_part = client_socket.recv(1024)
-        message = b""
-        while message_part:
-            message += message_part
-            message_part = client_socket.recv(1024)
-        logging.info("Got '{0}' from {1}".format(message.decode("utf-8"),
-                                                 addr))
-        client_socket.send(message)
-        client_socket.close()
+
+        cthread = client_thread(client_socket)
+        cthread.run()
+
 
 if __name__ == '__main__':
     main()
